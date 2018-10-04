bb9090bda17b7fad570e8e3a2dacc9f0c293ad6d
Skye Brown <skye.brown@uky.edu>
2018-10-04 16:19:45 -0400

    Create restaurant model, add constants file

---
+++ QR-Scanner/Model/Constant.swift
@@ -0,0 +1,12 @@
+//
+//  Constant.swift
+//  QR-Scanner
+//
+//  Created by Skye Brown on 10/4/18.
+//  Copyright © 2018 Skye Brown. All rights reserved.
+//
+
+import Foundation
+
+//url constants
+let BASE_URL = ""
---
+++ QR-Scanner/Model/Restaurant.swift
@@ -0,0 +1,19 @@
+//
+//  Restaurant.swift
+//  QR-Scanner
+//
+//  Created by Skye Brown on 10/4/18.
+//  Copyright © 2018 Skye Brown. All rights reserved.
+//
+
+import Foundation
+
+struct Restaurant {
+    var name: String
+    var uid: String
+    
+    init(name: String, uid: String) {
+        self.name = name
+        self.uid = uid
+    }
+}
